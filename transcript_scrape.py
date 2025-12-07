0a091275d2490727a7015ae12aa5a3e7db1e76af
r-esty <romeodesty@gmail.com>
2025-12-07 16:42:01 +0000

    Updated Logic

--- transcript_scrape.py
+++ transcript_scrape.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 import json
 
+import os
+from dotenv import load_dotenv,dotenv_values
+
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
@@ -9,8 +12,12 @@ def link():
     if request.method == "POST" :
         link = request.form.get("link")
         print(link)
-
+       
 
     return render_template("index.html")
+
+load_dotenv()
+
+print(os.getenv("api_key"))
 
 
