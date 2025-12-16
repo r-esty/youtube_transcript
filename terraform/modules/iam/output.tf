5ac52e028f13045fc3eec7a41cc2170e07cb443c
r-esty <romeodesty@gmail.com>
2025-12-16 00:43:31 +0000

    Add iam module

---
+++ terraform/modules/iam/output.tf
@@ -0,0 +1,9 @@
+output "ecs_task_execution_role" {
+  description = "ID of the ecs_task_execution_role"
+  value       = aws_iam_role.ecs_task_execution_role.id
+}
+
+output "ecs_execution_role_policy" {
+  description = "ID of the ecs_execution_role_policy"
+  value       = aws_iam_role_policy_attachment.ecs_execution_role_policy.id
+}
